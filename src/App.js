4713785e2abc5f02c02162537d1e97788edc8ec1
AytacSahin <haytacsahin@gmail.com>
2023-04-30 04:06:54 +0300

    S6G2 Tamamlandı.

--- src/App.js
+++ src/App.js
@@ -5,18 +5,40 @@
 */
 
 // State hook u import edin
-import React from 'react';
+import React, { useState } from 'react';
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
-import './App.css';
+
+import sahteVeri from "./sahte-veri";
+import Gonderiler from "./bileşenler/Gonderiler/Gonderiler.js";
+import AramaCubugu from "./bileşenler/AramaCubugu/AramaCubugu.js";
+
+import "./App.css";
 
 const App = () => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
-	
-  const gonderiyiBegen = gonderiID => {
+  const gonderiyiBegen = (gonderiID) => {
+    // setGonderiler(
+    //   gonderiler.map((item) => {
+
+    //   })
+    // )
+
+    setGonderiler(
+      gonderiler.map((item) => {
+        if (item.id === gonderiID) {
+          item.likes++;
+          return item;
+        } else {
+          return item;
+        }
+      })
+    );
     /*
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
@@ -28,12 +50,15 @@ const App = () => {
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
+
   };
 
   return (
     <div className='App'>
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
+      <AramaCubugu/>
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
+      <Gonderiler gonderiBegeniFonk={gonderiyiBegen} gonderiListesi={gonderiler} />
     </div>
   );
 };
--- src/bileşenler/Yorumlar/Yorumlar.js
+++ src/bileşenler/Yorumlar/Yorumlar.js
@@ -8,7 +8,14 @@ const Yorumlar = props => {
 
   return (
     <div>
-      {/* her gönderi yorumları için map'le işleyerek bir Yorum bileşeni döndürün (proplarına dikkat ederek)*/}
+    {/* her gönderi yorumları için map'le işleyerek bir Yorum bileşeni döndürün (proplarına dikkat ederek)*/}
+    {yorumlar.map((e) => {
+      return (
+        <div>
+          <Yorum yorum={e}/>
+        </div>
+      );
+      })}
     </div>
   );
 };
